833f023a9cb72d6ce3d2bbe185e63843b3398087
Jeppe <jeppe@fribsen.net>
2022-11-13 12:20:36 +0100

    Removed reference to rainy output widget

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from .item import Item, ItemSW5e
 from .monster import Monster, Monster35, MonsterSW5e
-from .output_widget import RainyTextWidget
 from .signals import sNexus
 from .spell import Spell, PowerSW5e
 from .system import System
